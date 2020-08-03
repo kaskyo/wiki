f7152b60041f8dfb1429869d22aa93c01b246ab2
kaskyo <andruha40k@gmail.com>
2020-08-03 21:24:37 +0300

    Made new and edit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name='index'),
+    path("new", views.new, name='new'),
+    path("random", views.page, name='random'),
+    path("<str:title>/edit", views.edit, name='edit'),
+    path("<str:title>", views.page, name='page')   
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
+class NewPage(forms.Form):
+    title = forms.CharField(label="title")
+    text = forms.CharField(label="text", widget=forms.Textarea)
+    # title.widget.attrs.update(size='40')
+
+class EditPage(forms.Form):
+    text = forms.CharField(label="text", widget=forms.Textarea)
+    
 from . import util
 
 
@@ -8,3 +20,42 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    return render(request, "encyclopedia/page.html", {
+        "title": title,
+        "text": util.get_entry(title)
+    })
+
+def search(request):
+    return 1
+
+
+def edit(request, title):
+    if request.method == "GET":
+        return render(request, "encyclopedia/edit.html", {
+            "form": EditPage(initial={'text': util.get_entry(title)}),
+            "title": title
+        })
+    else:
+        form = EditPage(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            util.save_entry(title,text)
+            # return HttpResponseRedirect(reverse('page', title))
+            return redirect('page', title)
+        else:
+            return render(request, "encyclopedia/edit.html", {
+                'title': title,
+                'form': form
+            })
+
+def new(request):
+    form = NewPage(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        title = form.cleaned_data['title']
+        text = form.cleaned_data['text']
+        util.save_entry(title, text)
+        return redirect('page', title)
+    return render(request, "encyclopedia/new.html", {
+        'form': NewPage()
+    })
